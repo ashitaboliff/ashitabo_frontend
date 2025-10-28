34e2523004f9e085b4b4c1089f864dbbd723a585
watabegg <watabegg@gmail.com>
2025-10-28 13:30:20 +0900

    fix: 非同期やめた、どうだ...！

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import Link from 'next/link'
-// import { getImageUrl } from '@/shared/lib/r2'
+import { getImageUrl } from '@/shared/lib/r2'
 
 export const metadata: Metadata = {
 	title: '404',
@@ -19,26 +19,28 @@ const NotFoundImages: { id: number; src: string; score: number }[] = [
 	{ id: 10, src: '/error/404/10.webp', score: 135 },
 ]
 
-export default async function NotFound() {
-	const selectImage = async () => {
-		const random = Math.floor(Math.random() * 1000)
-		let cumulativeScore = 0
+export default function NotFound() {
+	const random = Math.floor(Math.random() * 1000)
+	let cumulativeScore = 0
+	let selectedImage = NotFoundImages[0].src
 
-		for (const image of NotFoundImages) {
-			cumulativeScore += image.score
-			if (random < cumulativeScore) {
-				return image.src
-			}
+	for (const image of NotFoundImages) {
+		cumulativeScore += image.score
+		if (random < cumulativeScore) {
+			selectedImage = image.src
+			break
 		}
-		return NotFoundImages[0].src
 	}
-
-	const selectedImage = await selectImage()
 
 	return (
 		<div className="flex flex-col items-center justify-center text-center">
 			<div className="mb-8">
-				<img src={selectedImage} alt="404 Not Found" width={400} height={225} />
+				<img
+					src={getImageUrl(selectedImage)}
+					alt="404 Not Found"
+					width={400}
+					height={225}
+				/>
 			</div>
 			<h1 className="text-4xl font-bold mb-4">Page Not Found</h1>
 			<p className="text-lg mb-2">お探しのページは見つかりませんでした。</p>
